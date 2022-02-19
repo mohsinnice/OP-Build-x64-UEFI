9e74bd3849788e375e7c2d2344e74507074e98c4
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2022-02-18 23:19:18 -0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -5,7 +5,7 @@
 # Author: 0xACE7
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.199.77/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.199.99/g' package/base-files/files/bin/config_generate
 
 #2. Modify Hostname
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenwrtBP'' package/lean/default-settings/files/zzz-default-settings
