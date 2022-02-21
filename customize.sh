f953e13585f87e062080a6f053013df57c22dc4c
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2022-02-21 01:33:53 -0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -11,7 +11,7 @@ sed -i 's/192.168.1.1/192.168.199.99/g' package/base-files/files/bin/config_gene
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenwrtBP'' package/lean/default-settings/files/zzz-default-settings
 
 #3. 设置密码为空
-sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$4xKZB45Q$w0CPT5M6vBWbYNmSWuxfU.:19007:0:99999:7:::/g' /etc/shadow
+sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$4xKZB45Q$w0CPT5M6vBWbYNmSWuxfU.:19007:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
 
 #4. Modify builder
 sed -i "s/OpenWrt /0xACE7 build $(TZ=UTC-3 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
@@ -20,11 +20,22 @@ sed -i "s/OpenWrt /0xACE7 build $(TZ=UTC-3 date "+%Y.%m.%d") @ OpenWrt /g" packa
 #sed -i 's/"Argon 主题设置"/"主题设置"/g' feeds/kenzo/luci-app-argon-config/po/zh-cn/argon-config.po
 sed -i 's/"Argonne 主题设置"/"主题设置"/g' feeds/small8/luci-app-argonne-config/po/zh-cn/argonne-config.po
 mv feeds/small8/luci-theme-argonne/htdocs/luci-static/argonne/img/argon.svg feeds/small8/luci-theme-argonne/htdocs/luci-static/argonne/img/argonne.svg
-sed -i 's/"Turbo ACC 网络加速设置"/"网络加速"/g' package/lean/luci-app-turboacc/po/zh-cn/turboacc.po
+sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' feeds/luci/applications/luci-app-turboacc/po/zh-cn/turboacc.po
 
 #6. Change dns server
 sed -i "2i # network config" package/lean/default-settings/files/zzz-default-settings
 sed -i "3i uci set network.lan.dns='127.0.0.1'"  package/lean/default-settings/files/zzz-default-settings
+sed -i "4i uci set network.lan.delegate='0'" package/lean/default-settings/files/zzz-default-settings
+sed -i "5i uci set network.lan.gateway='192.168.199.1'" package/lean/default-settings/files/zzz-default-settings
+sed -i "6i # ipv6 network config" package/lean/default-settings/files/zzz-default-settings
+sed -i "7i uci set network.lan6='interface'" package/lean/default-settings/files/zzz-default-settings
+sed -i "8i uci set network.lan6.proto='dhcpv6'" package/lean/default-settings/files/zzz-default-settings
+sed -i "9i uci set network.lan6.ifname='eth0'" package/lean/default-settings/files/zzz-default-settings
+sed -i "10i uci set network.lan6.delegate='0'" package/lean/default-settings/files/zzz-default-settings
+sed -i "11i uci set network.lan6.reqaddress='try'" package/lean/default-settings/files/zzz-default-settings
+sed -i "12i uci set network.lan6.reqprefix='auto'" package/lean/default-settings/files/zzz-default-settings
+sed -i "13i uci set network.lan6.peerdns='0'" package/lean/default-settings/files/zzz-default-settings
+sed -i "14i uci set network.lan6.dns='::1'" package/lean/default-settings/files/zzz-default-settings
 
 #7. Replacement Argon to main theme
 #sed -i 's/default Bootstrap theme/default Argon theme/g' feeds/luci/collections/luci/Makefile
