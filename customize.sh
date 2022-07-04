b3b8c25a62bfda5108784a9700c036b331d23c85
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2022-07-04 03:49:30 -0700

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -5,13 +5,13 @@
 # Author: 0xACE7
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.199.99/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.199.99/g' package/base-files/files/bin/config_generate
 
 #2. Modify Hostname
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenwrtBP'' package/lean/default-settings/files/zzz-default-settings
 
 #3. 设置密码为空
-sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$4xKZB45Q$w0CPT5M6vBWbYNmSWuxfU.:19007:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
+#sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$4xKZB45Q$w0CPT5M6vBWbYNmSWuxfU.:19007:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
 
 #4. Modify builder
 sed -i "s/OpenWrt /0xACE7 build $(TZ=UTC-3 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
@@ -26,7 +26,7 @@ sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' feeds/luci/applications/luc
 sed -i "2i # network config" package/lean/default-settings/files/zzz-default-settings
 sed -i "3i uci set network.lan.dns='127.0.0.1'"  package/lean/default-settings/files/zzz-default-settings
 sed -i "4i uci set network.lan.delegate='0'" package/lean/default-settings/files/zzz-default-settings
-sed -i "5i uci set network.lan.gateway='192.168.199.1'" package/lean/default-settings/files/zzz-default-settings
+sed -i "5i uci set network.lan.gateway='192.168.1.1'" package/lean/default-settings/files/zzz-default-settings
 sed -i "6i # ipv6 network config" package/lean/default-settings/files/zzz-default-settings
 sed -i "7i uci set network.lan6='interface'" package/lean/default-settings/files/zzz-default-settings
 sed -i "8i uci set network.lan6.proto='dhcpv6'" package/lean/default-settings/files/zzz-default-settings
@@ -42,12 +42,12 @@ sed -i "14i uci set network.lan6.dns='::1'" package/lean/default-settings/files/
 #sed -i 's/+luci-theme-bootstrap/+luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 #8. Update dnscrypt-proxy2 2.0.45 to 2.1.1
-rm -rf feeds/packages/net/dnscrypt-proxy2
-git clone https://github.com/0xACE8/ace-packages.git feeds/packages/net/
+#rm -rf feeds/packages/net/dnscrypt-proxy2
+#git clone https://github.com/0xACE8/ace-packages.git feeds/packages/net/
 
 #8. Update hysteria 0.9.6 to 0.9.7
 #sed -i 's/"0.9.6"/"0.9.7"/g' feeds/passwall/hysteria/Makefile
 
 #10. Change to my banner
-sudo rm package/base-files/files/etc/banner
-wget https://raw.githubusercontent.com/0xACE8/OP-Build-x64-UEFI/main/banner -O package/base-files/files/etc/banner
+#sudo rm package/base-files/files/etc/banner
+#wget https://raw.githubusercontent.com/0xACE8/OP-Build-x64-UEFI/main/banner -O package/base-files/files/etc/banner
